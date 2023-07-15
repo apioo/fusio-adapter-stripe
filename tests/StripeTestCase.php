4e052bede08d1ee32e0567268a826a1a6d3ed8b5
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-07-15 19:38:27 +0200

    update

--- tests/StripeTestCase.php
+++ tests/StripeTestCase.php
@@ -29,7 +29,7 @@ use PHPUnit\Framework\TestCase;
  * StripeTestCase
  *
  * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
- * @license http://www.gnu.org/licenses/agpl-3.0
+ * @license http://www.apache.org/licenses/LICENSE-2.0
  * @link    https://www.fusio-project.org/
  */
 class StripeTestCase extends TestCase
